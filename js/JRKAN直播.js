f38e2ddfc83fb8a7c1d700d28660f93a934032d4
kerkier <huqiyao1990@163.com>
2023-10-25 11:12:20 +0800

    git

---
+++ js/JRKAN直播.js
@@ -0,0 +1,33 @@
+var rule = {
+    title:'JRKAN直播',
+    host:'http://www.jrkankan.com/?lan=1',
+   
+   
+    url:'/fyclass',
+    searchUrl:'',
+    searchable:0,
+    quickSearch:0,
+    class_name:'全部',
+    class_url:'/',
+   
+    headers:{
+        'User-Agent':'MOBILE_UA'
+    },
+    timeout:5000,
+    play_parse:true,
+    lazy:"",
+    limit:6,
+    double:false,
+    推荐:'*',
+   
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.d-touch');tabs.forEach(function(it){var pz=pdfh(it,'.name:eq(1)&&Text');var ps=pdfh(it,'.name:eq(0)&&Text');var pk=pdfh(it,'.name:eq(2)&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.lab_time&&Text');var url=pd(it,'a.me&&href');items.push({desc:timer+'🏆'+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
+    二级:{
+        "title":".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",
+        "img":"img&&src",
+        "desc":";;;.lab_team_home&&Text;.lab_team_away&&Text",
+        "content":".sub_list ul&&Text",
+        "tabs":"js:TABS=['JRKAN直播']",
+        "lists":"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});",
+    },
+    搜索:'',
+}
